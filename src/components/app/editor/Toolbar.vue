b5e9b1f3b84cd3a6a40e705067f3cb488ffc7188
EmersonBrSouza <emerson_souza_fsa@hotmail.com>
2018-06-25 10:16:54 -0300

    Toolbar color

--- src/components/app/editor/Toolbar.vue
+++ src/components/app/editor/Toolbar.vue
@@ -16,16 +16,58 @@
         </div>
       </div>
     </div>
-    <div class="column is-2">
+    <div class="column is-3">
       <div class="columns right-line">
-        <div class="column is-6 pr-0">
-          <b-field label="Estilo" custom-class="menu-label mb-5" size="is-small"><br>
-          </b-field>
+        <div class="column is-4 pr-0">
+          <div class="columns is-multiline">
+            <div class="column is-12 pb-0 mb-10">
+              <span class="label menu-label">Estilo</span>
+            </div>
+            <div class="column is-12 pt-0">
+              <button class="button">
+                <b-icon icon="bold"></b-icon>
+              </button>
+              <button class="button">
+                <b-icon icon="italic"></b-icon>
+              </button>
+              <button class="button">
+                <b-icon icon="underline"></b-icon>
+              </button>
+            </div>
+          </div>
         </div>
-        <div class="column is-6 pl-10">
-          <b-field label="Tamanho" custom-class="menu-label mb-5">
-            <b-input type="number" min="1"></b-input>
-          </b-field>
+        <div class="column is-5 pl-10">
+          <div class="columns is-multiline">
+            <div class="column is-12 pb-0 mb-10">
+              <span class="label menu-label">Alinhamento</span>
+            </div>
+            <div class="column is-12 pt-0">
+              <button class="button">
+                <b-icon icon="align-left"></b-icon>
+              </button>
+              <button class="button">
+                <b-icon icon="align-right"></b-icon>
+              </button>
+              <button class="button">
+                <b-icon icon="align-center"></b-icon>
+              </button>
+              <button class="button">
+                <b-icon icon="align-justify"></b-icon>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="column is-3 pl-10">
+          <div class="columns is-multiline">
+            <div class="column is-12 pb-0 mb-10">
+              <span class="label menu-label">Cor</span>
+            </div>
+            <div class="column is-12 pt-0">
+              <button class="button">
+                <b-icon icon="palette"></b-icon>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
